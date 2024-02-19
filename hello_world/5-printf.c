d813589ca1909e46d20b42eba11e7d5366e7a404
Bakhish85 <umudlu85@gmail.com>
2024-02-19 08:13:45 +0000

    Printf function

---
+++ hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - The main entry point
+ *
+ * Description: Shows the results of printf function
+ *
+ * Return: Always (0) Succesful
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
