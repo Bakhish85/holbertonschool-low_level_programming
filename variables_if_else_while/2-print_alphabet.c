a1f024102244f0746dc8a8fcc7f24393e52bef37
Bakhish85 <umudlu85@gmail.com>
2024-02-19 11:55:04 +0000

    THe alpabet

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - The entry point
+ *
+ * Description: Prints the alphabet in lowercase
+ *
+ * Return: Always 0 Success
+ */
+int main(void)
+{
+	char c;
+
+	for (c = 'a', c <= 'z', c++)
+		putchar(c);
+		putchar(\n);
+	return (0);
+}
+
+
