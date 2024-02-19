5fa1b9da158fb3c52bba773b8371d21742d0f3d8
Bakhish85 <umudlu85@gmail.com>
2024-02-19 08:05:11 +0000

    Puts function

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,6 +1,13 @@
 #include<stdio.h>
-int main(void) 
+/**
+ * main - This is the entry point of the code
+ *
+ * Description: This function prints the input
+ *
+ * Return: always (0) Succesful
+ */
+int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	return(0);
+	return (0);
 }
