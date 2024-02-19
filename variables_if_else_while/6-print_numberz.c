23c766e2db0cf91f50747d5f0b674c39c8f6fda1
Bakhish85 <umudlu85@gmail.com>
2024-02-19 13:03:46 +0000

    Single digits with putchar

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - the entry point
+ *
+ * Description: single digits through putvhar function
+ *
+ * Return: Always 0 success
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0 ; i <= 9 ; i++)
+	{
+		putchar(i + 0);
+	}
+	putchar("\n")
+	return (0);
+}
+
