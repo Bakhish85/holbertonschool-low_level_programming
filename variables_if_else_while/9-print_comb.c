5dc85875ae98671ccd59ebf938d419ce3ebbfdaf
Bakhish85 <umudlu85@gmail.com>
2024-02-20 10:08:12 +0000

    possible combinations of single digits

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: Possible combinations of single digit numbers
+ *
+ * Return: Always 0 success
+ */
+int main(void)
+{
+	char c, b;
+
+	for (c = '0' ; c <= '9' ; c++)
+	{
+		for (b = '0' ; b <= '9' ; b++)
+			putchar(b);
+	}
+		putchar(c);
+	putchar('\n');
+	return (0);
+}
