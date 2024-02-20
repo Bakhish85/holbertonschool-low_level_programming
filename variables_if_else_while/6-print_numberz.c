84b63e0ed7d6388c76ff2e778b5a5e3edca9c5eb
Bakhish85 <umudlu85@gmail.com>
2024-02-20 08:45:17 +0000

    Digits with putchar

--- variables_if_else_while/6-print_numberz.c
+++ variables_if_else_while/6-print_numberz.c
@@ -8,13 +8,12 @@
  */
 int main(void)
 {
-	int i;
+	int c;
 
-	for (i = 0 ; i <= 9 ; i++)
+	for (c = 0 ; c <= 9 ; c++)
 	{
-		putchar(i + '0');
+		putchar(c + '0');
 	}
 	putchar("\n");
 	return (0);
 }
-
