e7426a96ffed155ea93665eaeb66375598e19fe0
Bakhish85 <umudlu85@gmail.com>
2024-02-20 09:14:35 +0000

    All the numbers of base 16

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: All the numbers of base 16 in lowercase followed by new line.
+ *
+ * Return: Always 0 Succes
+ */
+int main(void)
+{
+	int i;
+	char c;
+
+	for (i = 0 ; i <= 9 ; i++)
+	{
+		putchar((i % 10) + '0');
+	}
+	for (c = a ; c <= f ; c++)
+	{
+		putchar(c);
+	}
+	putchar('\n');
+}
