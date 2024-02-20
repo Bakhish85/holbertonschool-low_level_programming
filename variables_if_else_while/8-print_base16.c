6614b3f4b1d3d5e78d0849dc94ff7795cd8769ec
Bakhish85 <umudlu85@gmail.com>
2024-02-20 09:36:20 +0000

    numbers of base 16

--- variables_if_else_while/8-print_base16.c
+++ variables_if_else_while/8-print_base16.c
@@ -8,12 +8,15 @@
  */
 int main(void)
 {
-	int num;
-	char letter;
+	char c;
 
-	for (num = 0 ; num <= 9 ; num++)
-		putchar((num % 10) + '0');
-	for (letter = 'a' ; letter <= 'f' ; letter++)
-		putchar(letter);
+	for (c = '0' ; c <= '9' ; c++)
+	{
+		putchar(c);
+	}
+	for (c = 'a' ; c <= 'f' ; c++)
+	{
+		putchar(c);
+	}
 	putchar('\n');
 }
