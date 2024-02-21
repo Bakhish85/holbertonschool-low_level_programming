43d5bfb4e7b943dcd54e21164b7a6d7bfbcee85b
Bakhish85 <umudlu85@gmail.com>
2024-02-21 07:31:04 +0000

    Print putchar

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - The entry point
+ *
+ * Description: To print exactly _putchar
+ *
+ * Return: Always 0 succes
+ */
+int main(void)
+{
+	printf("_putchar\n");
+	return (0);
+}
