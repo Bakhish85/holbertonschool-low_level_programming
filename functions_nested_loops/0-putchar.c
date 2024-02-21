fa7dd14078efedf9ce0de07ff68458013774299c
Bakhish85 <umudlu85@gmail.com>
2024-02-21 08:07:19 +0000

    Print putchar

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,4 +1,6 @@
+#include "main.h"
 #include <stdio.h>
+
 /**
  * main - The entry point
  *
@@ -6,8 +8,27 @@
  *
  * Return: Always 0 succes
  */
-int main(void)
+
+int _putchar(char c)
 {
-	printf("_putchar\n");
-	return (0);
+	    return putchar(c);
+}
+
+int print_putchar()
+{
+	    _putchar('_');
+	    _putchar('p');
+	    _putchar('u');
+	    _putchar('t');
+	    _putchar('c');
+	    _putchar('h');
+	    _putchar('a');
+	    _putchar('r');
+	    _putchar('\n');
+	    return 0;
+}
+
+int main() {
+	    print_putchar();
+	    return 0;
 }
