4850768c779afa135eb7365300f4174acd843f6c
Bakhish85 <umudlu85@gmail.com>
2024-02-21 08:23:38 +0000

    Print alphabet

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - Check the code
+ *
+ * Description: Write the alphabet
+ *
+ * Return: Always 0 succesful
+ */
+
+int main(void)
+{
+	print_alphabet();
+	return (0);
+}
