fe60e933f0ad6cea4243d3c38cb188014e0255f4
Bakhish85 <umudlu85@gmail.com>
2024-02-24 09:37:40 +0000

    The last digit

---
+++ functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_last_digit - Returns the value of last digit
+ * @n: Tzhe number to be checked
+ * Return: the last digit
+ */
+int print_last_digit(int n)
+{
+	int last_digit = n % 10;
+
+	return (last_digit);
+}
