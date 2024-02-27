89bbfab77b6a726cc88cb86bbf128622e61a7004
Bakhish 85 <umudlu85@gmail.com>
2024-02-27 06:58:28 +0000

    Checks digit

---
+++ more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * _isdigit - Checks if the element is digit
+ * @c: The element to be checked
+ * Return: 1 if c is digit, otherwise 0
+ */
+int _isdigit(int c)
+{
+	if (c >= 0 && c <= 9)
+		return (1);
+	else
+		return (0);
+}
+
