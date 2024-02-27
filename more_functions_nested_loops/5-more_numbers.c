8f77888ad4118b80a4b888280d12bd771c68e219
Bakhish 85 <umudlu85@gmail.com>
2024-02-27 08:13:34 +0000

    Print numbera 0-14 10 times

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -6,17 +6,16 @@
  */
 void more_numbers(void)
 {
-	int a = 0;
+	int a;
 	int b;
 
-	while (a < 10)
+	for (a = 0 ; a < 10 ; a++)
 	{
 		for (b = 0 ; b < 15 ; b++)
 		{
 			if ( b > 9)
-			_putchar ((b / 10) + '0');
+				_putchar ((b / 10) + '0');
+			_putchar((b % 10) + '0');
 		}
-		_putchar((b % 10) + '0');
+		_putchar('\n');
 	}
-	_putchar('\n');
-}
