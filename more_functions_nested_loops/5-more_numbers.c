7cec4efe67350de43eccbe24191aa44ea642a611
Bakhish 85 <umudlu85@gmail.com>
2024-02-27 07:45:08 +0000

    Print numbers 0-14 10 times

---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * more_numbers - Prints 10 times the numbers, from 0 to 14
+ * Return: void
+ */
+void more_numbers(void)
+{
+	int a = '0';
+	int c;
+
+	while (i < '10')
+	{
+		for (c = '0' ; c < '15' ; c++)
+			_puthcar(c);
+	}
+	_putchar(i);
+	_putchar('\n');
+}
