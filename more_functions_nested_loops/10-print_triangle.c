c59b6303a3d81b008db7cea3d382d2abbb1a1d5b
Bakhish85 <umudlu85@gmail.com>
2024-02-28 06:30:01 +0000

    Triangle

---
+++ more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_triangle - Prints triangle
+ * @size: Shows the size of a trÄiangle
+ * Return: void
+ */
+void print_triangle(int size)
+{
+	int hash, space;
+
+	if (size <= 0)
+		_putchar('\n');
+	else
+	{
+		for (hash = 1; hash <= size; hash++)
+		{
+			for (space = size - hash; space > 0; space--)
+				_putchar(' ');
+			for (space = 0; space < hash; space++)
+				_putchar('#');
+			if (hash == size)
+				continue;
+			_putchar('\n');
+		}
+	}
+}
