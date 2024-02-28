d33bc7fe2b765c25fd4f63943942bbae780a34f6
Bakhish85 <umudlu85@gmail.com>
2024-02-28 05:52:56 +0000

    Fizz Buzz

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -7,18 +7,18 @@
  *
  * Return: void
  */
-void main(void)
+int main(int)
 {
 	int i;
 
 	for (i = 1 ; i <= 100 ; i++)
 	{
 		if ((i % 3) == 0)
-			prÄintf('Fizz');
+			prÄintf(Fizz);
 		else if ((i % 5) == 0)
-			printf('Buzz');
+			printf(Buzz);
 		else if ((i % 3) == 0 && (i % 5) == 0);
-			printf('FizzBuzz');
+			printf(FizzBuzz);
 	}
 	printf('\n');
 }
