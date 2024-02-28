3f0dbf2fc6235018fc8b98188afac8bf38ef765f
Bakhish85 <umudlu85@gmail.com>
2024-02-28 05:57:41 +0000

    Fizz Buzz

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -5,21 +5,24 @@
  * Description: Print 1 to 100, multiples of 3 is Fizz,
  * multiples of 5 is Buzz.
  *
- * Return: void
+ * Return: always 0
  */
-int main(int)
+int main(void)
 {
 	int i;
 
 	for (i = 1 ; i <= 100 ; i++)
 	{
 		if ((i % 3) == 0)
-			prÄintf(Fizz);
+			prÄintf("Fizz");
 		else if ((i % 5) == 0)
-			printf(Buzz);
+			printf("Buzz");
 		else if ((i % 3) == 0 && (i % 5) == 0);
-			printf(FizzBuzz);
+			printf("FizzBuzz");
+		else
+			printf("%d" , i)
 	}
 	printf('\n');
+	return (0);
 }
 
