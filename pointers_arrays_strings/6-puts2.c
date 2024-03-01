32e26110377d3462c3b282c2d4ae122b15b763fe
Bakhish85 <umudlu85@gmail.com>
2024-03-01 09:34:01 +0000

    Print one another character

--- pointers_arrays_strings/6-puts2.c
+++ pointers_arrays_strings/6-puts2.c
@@ -10,7 +10,7 @@ void puts2(char *str)
 	int length = 0;
 	int index = 0;
 
-	while (s[index++])
+	while (str[index++])
 		length++;
 	for (index = 0 ; index < length ; index++)
 		_putchar(s[2 * index]);
