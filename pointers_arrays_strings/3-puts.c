7b5f196bb28ab0896be0b8e5892a5490e5da71bb
Bakhish85 <umudlu85@gmail.com>
2024-03-01 06:09:21 +0000

    Print the string

---
+++ pointers_arrays_strings/3-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * _puts - The function that writes string
+ *
+ * @str - The string to be written
+ *
+ * Return - void
+ */
+void _puts(char *str)
+{
+	printf("%s\n" , str);
+}
