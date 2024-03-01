2657b1669b46285cd2027054ecd522f95c0dd35a
Bakhish85 <umudlu85@gmail.com>
2024-03-01 08:33:05 +0000

    Reversing function

---
+++ pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * rev_string - The function that reverses the string
+ * @s: The string to be reversed
+ * Return: void
+ */
+void rev_string(char *s)
+{
+	int length = strlen(s);
+
+	for (int i = 0; i <= length / 2; i++)
+	{
+		int temp = s[i];
+
+		s[i] = s[length - i - 1];
+		s[length - i - 1] = temp;
+	}
+	_putchar('\n');
+}
