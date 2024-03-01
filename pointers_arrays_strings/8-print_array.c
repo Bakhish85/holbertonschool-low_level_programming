d1041aa93d2970ac27ead84428b695d4520f4f56
Bakhish85 <umudlu85@gmail.com>
2024-03-01 11:53:32 +0000

    Print second half of string

---
+++ pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_array - prints n elements of an array of integers
+ * @n: the number of elements of the array
+ * @a: The array
+ * Return: void
+ */
+void print_array(int *a, int n)
+{
+	int index = 0;
+
+	for (index = 0; index < n; index++)
+		printf("%d", a[index]);
+		if (index == n - 1)
+			continue;
+		printf(", ");
+}
+
