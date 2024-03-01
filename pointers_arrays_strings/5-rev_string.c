bc18e3351b7dadc4f7950018b59d4b5dd8d8615a
Bakhish85 <umudlu85@gmail.com>
2024-03-01 09:07:56 +0000

    String reversing function

--- pointers_arrays_strings/5-rev_string.c
+++ pointers_arrays_strings/5-rev_string.c
@@ -8,15 +8,15 @@
 void rev_string(char *s)
 {
 	int length = 0;
-	int i;
+	int i = 0;
+	char temp;
 
 	while (s[i++])
 		length++;
-
-	for (i = 0; i <= length / 2; i++)
+		
+	for (i = length - 1; i <= length / 2; i--)
 	{
-		int temp = s[i];
-
+		temp = s[i];
 		s[i] = s[length - i - 1];
 		s[length - i - 1] = temp;
 	}
