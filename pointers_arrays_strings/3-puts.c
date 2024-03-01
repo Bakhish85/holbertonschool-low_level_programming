8f70da1ca5fc2b626471defc35789bdfef2425c1
Bakhish85 <umudlu85@gmail.com>
2024-03-01 07:15:47 +0000

    Print string

--- pointers_arrays_strings/3-puts.c
+++ pointers_arrays_strings/3-puts.c
@@ -1,15 +1,13 @@
 #include <stdio.h>
 #include "main.h"
 /**
- * _puts - The function that writes string
- *
- * @str - The string to be written
- *
- * Return - void
+ * _puts - The function that writes the string
+ * @str: The string to be written
+ * Return: void
  */
 void _puts(char *str)
 {
-	while(*str)
+	while (*str)
 		_putchar(*str++);
 		_putchar('\n');
 }
