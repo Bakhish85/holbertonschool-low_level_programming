58efdde436c1065c15e2cd92fc45828944d9caa9
Bakhish85 <umudlu85@gmail.com>
2024-03-07 08:06:29 +0000

    Print the name

---
+++ argc_argv/README.md
@@ -0,0 +1,1 @@
+#this folder is about argv and argc
---
+++ argc_argv/main.h
@@ -0,0 +1,13 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+ * File: main.h
+ *
+ * Desc: Header file containing declarateions for all functions
+ *
+ */
+
+int _putchar(char c);
+
+#endif
