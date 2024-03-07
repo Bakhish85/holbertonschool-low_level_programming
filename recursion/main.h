82c8acc9d3c721e47fa330d76add1f2deee0ef13
Bakhish85 <umudlu85@gmail.com>
2024-03-07 06:49:43 +0000

    Print a string

---
+++ recursion/main.h
@@ -0,0 +1,12 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/** Fİle: main.h
+ *
+ * Desc: Header file containing declarations for all functions
+ */
+
+int _putchar(char c);
+void _puts_recursion(char *s);
+
+#endif
