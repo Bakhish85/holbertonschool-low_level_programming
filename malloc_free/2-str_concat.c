e16186ce5f7b574011a207702b75972abff3b37b
Bakhish85 <umudlu85@gmail.com>
2024-03-16 07:59:24 +0000

    Concateanate two strings

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -9,35 +9,24 @@
  */
 char *str_concat(char *s1, char *s2)
 {
-	int i = 0, j = 0, k = 0, l = 0;
-	char *s;
+	int index;
+	int concat_index = 0;
+	int len = 0;
+	char *concat_str;
 
 	if (s1 == NULL)
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
-	while (s1[i])
-		i++;
-	while (s2[j])
-		j++;
-	l = i + j;
-	s = malloc((sizeof(char) * l) + 1);
+	for (index = 0; s1[index] || s2[index]; index++)
+		len++;
+	concat_str = malloc(sizeof(char) * len);
 
-	if (s == NULL)
+	if (concat_str == NULL)
 		return (NULL);
-	j = 0;
-
-	while (k < 1)
-	{
-		if (k <= i)
-			s[k] = s1[k];
-		if (k >= i)
-		{
-			s[k] = s2[j];
-			j++;
-		}
-		k++;
-	}
-	s[k] = '\0';
-	return (s);
+	for (index = 0; s1[index]; index++)
+		concat_str[concat_index++] = s1[index];
+	for (index = 0; s2[index]; index++)
+		concat_str[concat_index++] = s2[index];
+	return (concat_str);
 }
