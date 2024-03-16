f4e56e022dafc42753e4077bb173b8c78f519f9e
Bakhish85 <umudlu85@gmail.com>
2024-03-16 08:54:12 +0000

    Allocate memory

---
+++ more_malloc_free/main.h
@@ -0,0 +1,14 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+ * File: main.h
+ *
+ * Desc: Header file containing decalarations for all functions
+ * 
+ */
+
+int _putchar(char c);
+void *malloc_checked(unsigned int b);
+
+#endif
