724387f997a0d117f587f435e2cca8b641ec533c
Bakhish85 <umudlu85@gmail.com>
2024-03-16 08:17:02 +0000

    Free a 2 dimensional array

--- malloc_free/4-free_grid.c
+++ malloc_free/4-free_grid.c
@@ -14,11 +14,4 @@ void free_grid(int **grid, int height)
 		free(grid[index]);
 	free(grid);
 
-void free_grid(int **grid, int height)
-{
-	int index;
-
-	for (index = 0; index < height; index++)
-		free(grid[index]);
-	free(grid);
 }
