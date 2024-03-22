8dde3c9fa17eec435ef1ed8579bb527076fe2986
Bakhish85 <umudlu85@gmail.com>
2024-03-22 10:40:03 +0000

    Print a name

--- function_pointers/0-print_name.c
+++ function_pointers/0-print_name.c
@@ -3,8 +3,9 @@
  * print_name - Prints a name.
  * @name: The name to be printed.
  * @f: A pointer to a function that prints a name
+ * Return: Nothing
  */
-void print_nmae(char *name, void (*f)(char *))
+void print_name(char *name, void (*f)(char *))
 {
 	if (name == NULL || f == NULL)
 		return;
