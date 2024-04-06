2287fdab348ac3a68d76ccea7e20c3d1c4d1ca0a
Bakhish85 <umudlu85@gmail.com>
2024-04-06 06:28:07 +0000

    Print list

---
+++ singly_linked_lists/lists.h
@@ -0,0 +1,13 @@
+#ifndef LISTS_H
+#define LISTS_H
+/*
+ * File: lists.h
+ * Auth: Bakhish Umudlu
+ * Desc: Header file containing prototypes for all functions
+ *	used in the singly_linked_lists directory
+ */
+#include <stdlib.h>
+
+size_t print_list(const list_t *h);
+
+#endif
