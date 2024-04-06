51c18a1ff3a8d1295dab6e0e6fa86362eb50d458
Bakhish85 <umudlu85@gmail.com>
2024-04-06 06:55:19 +0000

    print list

--- singly_linked_lists/lists.h
+++ singly_linked_lists/lists.h
@@ -16,7 +16,6 @@
  * @str: string - (malloc'ed string)
  * @len: length of the string
  * @next: points to the next node
- * 
  * Description: singly linked list node structure
  */
 typedef struct list_s
